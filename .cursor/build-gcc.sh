5c53f629011914ee18606c38df7f58140eb96d6d
Jian Li <hellothedreamer@126.com>
2026-09-09 13:40:57 +0800

    Enable Fortran and LTO in the Cloud GCC configure

--- .cursor/build-gcc.sh
+++ .cursor/build-gcc.sh
@@ -6,16 +6,23 @@ BUILD="${GCC_BUILD_DIR:-${HOME}/gcc-build}"
 PREFIX="${GCC_PREFIX:-${HOME}/gcc-install}"
 JOBS="${GCC_JOBS:-2}"
 
+CONFIGURE_ARGS=(
+  --prefix="$PREFIX"
+  --enable-languages=c,c++,fortran,lto
+  --enable-lto
+  --disable-multilib
+  --disable-bootstrap
+  --disable-nls
+)
+
 mkdir -p "$BUILD"
 cd "$BUILD"
 
-if [[ ! -f Makefile ]]; then
-  "$ROOT/configure" \
-    --prefix="$PREFIX" \
-    --enable-languages=c,c++ \
-    --disable-multilib \
-    --disable-bootstrap \
-    --disable-nls
+stamp="$BUILD/cursor-configure.stamp"
+desired="$(printf '%s\n' "${CONFIGURE_ARGS[@]}")"
+if [[ ! -f Makefile ]] || [[ "$(cat "$stamp" 2>/dev/null || true)" != "$desired" ]]; then
+  "$ROOT/configure" "${CONFIGURE_ARGS[@]}"
+  printf '%s\n' "$desired" > "$stamp"
 fi
 
 make -j"$JOBS"
